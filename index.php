9cdeb25a6975739c6b0f7c1a05b9f27071d28cfb
Feniux <acfantasycity@gmail.com>
2017-04-03 20:58:03 -0300

    Exclude .zip

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 require 'simple_html_dom/simple_html_dom.php';
 $html = file_get_html("http://www.azulviagens.com.br/");
 
-foreach($html->find('label') as $j);
-foreach($html->find('select') as $i);
+foreach($html->find('p') as $j);
+
 echo $j;
-echo $i;
+
 
 
 
