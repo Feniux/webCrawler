b471640645cca49afa48b1e348ee95c0bc8c47da
Feniux <acfantasycity@gmail.com>
2017-04-03 23:14:14 -0300

    Teste de script --- voegol

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <?php
 require 'simple_html_dom/simple_html_dom.php';
-$html = file_get_html("http://www.azulviagens.com.br/");
+$html = file_get_html("https://www.voegol.com.br/pt/sua-viagem/ofertas/ofertas-da-semana");
+/*$html = file_get_html("https://www.latam.com/pt_br/");*/
 
-foreach($html->find('p') as $j);
+foreach($html->find('table') as $table);
+/*foreach ($html->find('div') as $div);*/
 
-echo $j;
+echo $table;
+/*echo $div;*/
 
 
-
-
+//extract data from the post extract($_POST); 
+//set POST variables 
+//URL IS SET, I HAVE JUST TOOK IT OUT OF THIS CODE SNIPPET 
+/*$urltopost = 'https://www.azultravel.com.br/travel/arc_waiting.cfm'; 
+$datatopost = array ( 's_3_2_5_0' => 0130016825566667, 'SWEFo' => 'SWEForm3_0', 
+	'SWEField' => 's_3_1_0_0', 'SWENeedContext' => 'true', 'W' => 't', 'SWECmd' => 'InvokeMethod', 
+	'SWEMethod' => 'SearchForLicence', 'SWESP' => 'false', 'SWEDIC' => 'false', 'SWEReqRowId' => '0',
+	'SWEView' => 'LDL+Licence+Query+View','SWEC' => '3','SWEBID' => '1363349905','SWEGHOTO' => 'true', 
+	'SWETS' => '1363349913267', 'SWEApplet' => 'LDL+Query+Licences'); 
+$ch = curl_init ($urltopost); 
+curl_setopt ($ch, CURLOPT_POST, true); 
+curl_setopt ($ch, CURLOPT_POSTFIELDS, $datatopost); 
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0); 
+$returndata = curl_exec ($ch); 
+echo $returndata; */
 
 
 
