fd3e186236eadceb6f1172707cb8ff3bed02dbc1
Feniux <acfantasycity@gmail.com>
2017-04-06 13:19:28 -0300

    Atualização de repo

--- avianca.php
+++ avianca.php
@@ -16,19 +16,24 @@ $array_result_avianca[] = '<thead>'.
 						'<tbody>';
 
 for($i = 0; $i < count($ofer[1]); $i++){
-	
-	$aux = $ofer[1][$i];
 
-	for($j = $i; $j < count($ofer[1])-$j; $j++) if($ofer[1][$j] == $aux) $s = 1;
-
-	if($s != 0)
+	if($i == 0)
 		array_push($array_result_avianca, 
 			'<tr>'.
 				'<td>'.$ofer[1][$i].'</td>'.
 			'</tr>'
 		);
 
-	
+	for($j = $i; $j < count($ofer[1])-$j; $j++) if($ofer[1][$j] == $ofer[1][$i]) $s = 1;
+
+	if($s == 0)
+		array_push($array_result_avianca, 
+			'<tr>'.
+				'<td>'.$ofer[1][$i].'</td>'.
+			'</tr>'
+		);
+
+	$s = 0;
 }
 
 array_push($array_result_avianca, '</tbody><tfooter></tfooter>');
